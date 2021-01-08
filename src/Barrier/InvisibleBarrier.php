a52906625a4f7fa456f48571fa247a18b670d7dd
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-01-07 17:54:53 -0800

    Update InvisibleBarrier.php

--- src/Barrier/InvisibleBarrier.php
+++ src/Barrier/InvisibleBarrier.php
@@ -2,32 +2,37 @@
 
 namespace Barrier;
 
-use pocketmine\plugin\PluginManger;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\utils\TextFormat as c;
 
-class InvisibleBarrier extends PlayerMoveEvent{
-	
-	public function onEnable() : void {
-		$this->getLogger()->notice(c::BOLD.c::DARK_AQUA."(!)".c::RESET.c::DARK_PURPLE." InvisibleBarrier by IceCruelStuff has been enabled");
+class InvisibleBarrier extends PluginBase implements Listener {
+
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		// $this->getLogger()->info(TextFormat::BOLD . TextFormat::DARK_AQUA . " InvisibleBarrier has been enabled");
 	}
-	
+
 	public function onMove(PlayerMoveEvent $event) {
-		if($this->isInside($event->getPlayer()->getPosition(), $pos1, $pos2)){
-			$event->getPlayer()->teleport($event->getFrom());
-			// inform players why they're knocked back
+		$player = $event->getPlayer();
+		if ($this->isInside($player->getPosition(), $pos1, $pos2)) {
+			$player->teleport($event->getFrom());
+			$player->sendMessage($this->getConfig()->get("message"));
 		}
 	}
-	
-	public function isInside(Position $position, Position $p1, Position $p2) {
-		return ($position->getX() >= $p1->getX() && $position->getX() <= $p2->getX() && $position->getY() >= $p2->getY() && $position->getY() <= $p1->getY() && $position->getZ() >= $p1->getZ() && $position->getZ() <= $p2->getZ() && $position->getLevel()->getName() === $p1->getLevel()->getName() $position->getLevel()->getName() === $p2->getLevel()->getName());
+
+	private function isInside(Position $position, Position $p1, Position $p2) {
+		return ($position->getX() >= $p1->getX() && $position->getX() <= $p2->getX() && $position->getY() >= $p2->getY() && $position->getY() <= $p1->getY() && $position->getZ() >= $p1->getZ() && $position->getZ() <= $p2->getZ() && $position->getLevel()->getName() === $p1->getLevel()->getName(), $position->getLevel()->getName() === $p2->getLevel()->getName());
 	}
-	
-	public function onDisable() : void {
-		$this->getLogger()->notice(c::BOLD.c::DARK_AQUA."(!)".c::RESET.c::DARK_PURPLE." InvisibleBarrier by IceCruelStuff has been enabled");
+
+	public function onDisable() {
+		//$this->getLogger()->info(TextFormat::BOLD . TextFormat::RED . "InvisibleBarrier has been disabled");
 	}
+
 }
